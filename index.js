a282b137f60dfab7f70c1fbff5817688c5583c52
Lopessccp <wesleysardella@gmail.com>
2025-08-28 11:54:59 -0300

    Desafio do Felipão dio.me

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("oi");
+let nome = "Espadachin da massa"
+let xp = 10001
+
+if (xp < 1000) {
+    console.log(nome + " é nível ferro")
+    } else if (xp >= 1001 && xp <= 2000) {
+        console.log(nome + " é nível bronze")
+    } else if (xp >= 2001 && xp <= 5000){
+        console.log(nome + " é nível prata")
+    } else if (xp >= 5001 && xp <= 7000){
+        console.log(nome + " é nível ouro")
+    } else if (xp >= 7001 && xp <= 8000){
+        console.log(nome + " é nível platina")
+    } else if (xp >= 8001 && xp <= 9000){
+        console.log(nome + " é nível Ascendente")
+    } else if (xp >= 9001 && xp <= 10000){
+        console.log(nome + " é nível Imortal")
+    } else if (xp >= 10001) 
+        console.log(nome + " é nível Radiante")
